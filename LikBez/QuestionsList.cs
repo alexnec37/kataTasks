fd98305d37d988a2d1fb26c81c4406e3da86cd05
nas <alexnec@inbox.ru>
2020-02-02 00:09:08 +0700

    вопросики

--- LikBez/QuestionsList.cs
+++ LikBez/QuestionsList.cs
@@ -9,7 +9,10 @@ namespace LikBez
         public IList<string> InitList()
         {
             IList<string> Questionslist = new List<string>();
+            Questionslist.Add("2   Double и Decimal");
             Questionslist.Add("4   Ref и Out");
+            Questionslist.Add("13  Сжатие до выражения с№ 7");
+            Questionslist.Add("14  Индексаторы");
             Questionslist.Add("18  Ссылочные преобразования as и is");
 
             return Questionslist;
