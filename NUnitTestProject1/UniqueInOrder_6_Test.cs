f49328c13b879b6d7cd8fa035fe49560f14c4586
nas <alexnec@inbox.ru>
2020-02-09 18:59:16 +0700

    Unique In Order 6kyu

---
+++ NUnitTestProject1/UniqueInOrder_6_Test.cs
@@ -0,0 +1,27 @@
+﻿using codewars;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestProject1
+{
+    public class UniqueInOrder_6_Test
+    {
+
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test]
+            public void EmptyTest()
+            {
+                Assert.AreEqual("", UniqueInOrder_6.UniqueInOrder(""));
+            }
+            [Test]
+            public void Test1()
+            {
+                Assert.AreEqual("ABCDAB", UniqueInOrder_6.UniqueInOrder("AAAABBBCCDAABBB"));
+            }
+        }
+    }
+}
---
+++ codewars/UniqueInOrder_6.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace codewars
+{
+    public class UniqueInOrder_6
+    {
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            T item, last = default(T);
+            var list = new List<T>();
+
+            using (var enumerator = iterable.GetEnumerator())
+            {
+                if (enumerator.MoveNext())
+                {
+                    last = enumerator.Current;
+                    list.Add(last);
+                }
+                
+                while(enumerator.MoveNext())
+                {
+                    item = enumerator.Current;
+                    if (item.Equals(last))
+                    {
+                        continue;
+                    }
+
+                    list.Add(item);
+                    last = item;
+                }
+            }
+
+            return list;
+        }
+    }
+}
