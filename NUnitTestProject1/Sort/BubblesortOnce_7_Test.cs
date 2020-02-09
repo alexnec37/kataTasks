2b095d91fb9eb0f1eb74f8ad5e40429a8db81105
nas <alexnec@inbox.ru>
2020-02-09 21:15:44 +0700

    BubbleSortOnce 7

---
+++ NUnitTestProject1/Sort/BubblesortOnce_7_Test.cs
@@ -0,0 +1,22 @@
+﻿using codewars.Sort;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestProject1.Sort
+{
+    [TestFixture]
+    public class BubblesortOnce_7_Test
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            // Example test case from description
+
+            var expected = new int[] { 7, 5, 3, 1, 2, 4, 6, 8, 9 };
+            var actual = new int[] { 9, 7, 5, 3, 1, 2, 4, 6, 8 };
+            Assert.AreEqual(expected, BubblesortOnce_7.BubbleSortOnce(actual));
+        }
+    }
+}
---
+++ codewars/Sort/BubblesortOnce_7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codewars.Sort
+{
+    public class BubblesortOnce_7
+    {
+        public static int[] BubbleSortOnce(int[] input)
+        {
+            //var actual = new int[] { 9, 7, 5, 3, 1, 2, 4, 6, 8 };
+            int x;
+            for(int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input.Length - 1 - i; j++)
+                {
+                    if(input[j] > input[j+1])
+                    {
+                        x = input[j + 1];
+                        input[j + 1] = input[j];
+                        input[j] = x;
+                    }
+                }
+
+                break;
+            }
+
+            return input;
+        }
+    }
+}
