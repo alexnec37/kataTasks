df629256ee51a0f3a40d1765b0e65bb48e1c2d2c
nas <alexnec@inbox.ru>
2020-02-12 14:00:25 +0700

    intersect arrays

---
+++ NUnitTestProject1/Arrays/ArrayIntersectAll_6_Test.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using codewars.Arrays;
+
+namespace NUnitTestProject1.Arrays
+{
+    [TestFixture]
+    public class ArrayIntersectAll_6_Test
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var a = new string[] { "dog", "bar", "foo" };
+            var b = new string[] { "foo", "bar", "cat" };
+            var c = new string[] { "gin", "foo", "bar" };
+
+            Assert.AreEqual(new string[] { "bar", "foo" }, ArrayIntersectAll_6.Intersect(a, b, c));
+        }
+
+        [Test]
+        public void BasicTests2()
+        {
+            var a = new string[] { "dog" };
+            var b = new string[] { "foo" };
+            var c = new string[] { "gin", "foo" };
+
+            Assert.AreEqual(new string[] { "foo" }, ArrayIntersectAll_6.Intersect(a, b, c));
+        }
+    }
+}
---
+++ codewars/Arrays/ArrayIntersectAll_6.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codewars.Arrays
+{
+    public class ArrayIntersectAll_6
+    {
+        public static string[] Intersect(params string[][] arrays)
+        {
+            if (arrays.Length == 0) return new string[0];
+
+            string[] preArray;
+            preArray = arrays[0];
+
+            for (int i = 1; i < arrays.Length; i++)
+            {
+                preArray = IntersectArray(preArray, arrays[i]);
+                if(preArray.Length == 0)
+                {
+                    preArray = arrays[i];
+                }
+            }
+
+            return preArray;
+        }
+
+        static string[] IntersectArray(string[] a, string[] b)
+        {
+            int k = 0;
+            int size = a.Length > b.Length ? b.Length : a.Length;
+            string[] c = new string[size];
+
+            for(int i = 0; i < a.Length; i++)
+            {
+                for(int j = 0; j < b.Length; j++)
+                {
+                    if(a[i] == b[j])
+                    {
+                        c[k] = a[i];
+                        k++;
+                    }
+                }
+            }
+
+            Array.Resize(ref c, k);
+            return c;
+        }
+
+        static string[] IntersectSorted(string[] a, string[] b)
+        {
+            int size = a.Length > b.Length ? b.Length : a.Length;
+            string[] c = new string[size];
+            int i = 0, j = 0, k = 0;
+
+            while (i < a.Length || j < b.Length)
+            {
+                if (a[i].CompareTo(b[j]) > 0) 
+                    j++;
+                else if (a[i].CompareTo(b[j]) < 0)
+                    i++;
+                else
+                {
+                    c[k++] = a[i];
+                    if (i == a.Length - 1 || j == b.Length - 1)
+                        break;
+                    else
+                    {
+                        i++;
+                        j++;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Array.Resize(ref c, k);
+            return c;
+        }
+    }
+}
